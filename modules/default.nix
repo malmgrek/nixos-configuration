1a78e20a14f8da7e00d9daec69b766e473d8781d
Stratos Staboulis <stratos.staboulis@gmail.com>
2020-09-06 20:16:27 +0000

    Fix users

--- modules/default.nix
+++ modules/default.nix
@@ -4,12 +4,23 @@ let mkOptionStr = value: mkOption
   { type = types.str;
     default = value; };
 in {
+
+  imports = [];
   
   options = {
+
     my = {
       username = mkOptionStr "malmgrek";
       email = mkOptionStr "stratos.staboulis@gmail.com";
+      user = mkOption { type = types.submodule; };
     };
+
+  };
+
+  config = {
+
+    users.users.${config.my.username} = mkAliasDefinitions options.my.user;
+    
   };
 
 }
