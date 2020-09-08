49cffeaba9dcc7edb4c2325f46cee02ebd95d75a
Stratos Staboulis <stratos.staboulis@gmail.com>
2020-09-08 08:24:54 +0300

    Update channels script

--- channels.zsh
+++ channels.zsh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
-function add_channels() {
+function addNixOS() {
   nix-channel --add "https://nixos.org/channels/nixos-20.03" nixos
+}
+
+function addHomeManager() {
   nix-channel --add "https://github.com/rycee/home-manager/archive/release-20.03.tar.gz" home-manager
+}
+
+function addNixOSUnstable() {
   nix-channel --add "https://nixos.org/channels/nixpkgs-unstable" nixpkgs-unstable
+}
+
+function addAll() {
+  addNixOS
+  addHomeManager
+  addNixOSUnstable
 }
