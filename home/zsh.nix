cc1734637103df0df098232735790f050e3d24ce
Stratos Staboulis <stratos.staboulis@gmail.com>
2021-10-09 01:23:29 +0300

    Add zsh completions

--- home/zsh.nix
+++ home/zsh.nix
@@ -43,6 +43,7 @@
         zplug = {
           enable = true;
           plugins = [
+            { name = "zsh-users/zsh-completions"; }
             { name = "zsh-users/zsh-autosuggestions"; }
             { name = "zsh-users/zsh-syntax-highlighting"; tags = [ defer:2 ]; }
             { name = "hlissner/zsh-autopair"; tags = [ defer:2 ]; }
