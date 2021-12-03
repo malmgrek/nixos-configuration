1c07170953f2e8bc72a6e4af046e69bf379411f3
Stratos Staboulis <stratos.staboulis@gmail.com>
2021-12-03 09:30:08 +0200

    Add one light theme to Alacritty

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -9,24 +9,50 @@ let
     normal = {
       # NOTE: Use '#131613' for the `black` color if you'd like to see
       # black text on the background.
-      black = "#282c34";
-      red = "#e06c75";
-      green = "#98c379";
-      yellow = "#d19a66";
-      blue = "#61afef";
+      black =   "#282c34";
+      red =     "#e06c75";
+      green =   "#98c379";
+      yellow =  "#d19a66";
+      blue =    "#61afef";
       magenta = "#c678dd";
-      cyan = "#56b6c2";
-      white = "#abb2bf";
+      cyan =    "#56b6c2";
+      white =   "#abb2bf";
     };
     bright = {
-      black = "#5c6370";
-      red = "#e06c75";
-      green = "#98c379";
-      yellow = "#d19a66";
-      blue = "#61afef";
+      black =   "#5c6370";
+      red =     "#e06c75";
+      green =   "#98c379";
+      yellow =  "#d19a66";
+      blue =    "#61afef";
       magenta = "#c678dd";
-      cyan = "#56b6c2";
-      white = "#ffffff";
+      cyan =    "#56b6c2";
+      white =   "#ffffff";
+    };
+  };
+  onelight = {
+    primary = {
+      foreground = "#282c34";
+      background = "#ffffff";
+    };
+    normal = {
+      black =   "#282c34";
+      red =     "#e06c75";
+      green =   "#98c379";
+      yellow =  "#d19a66";
+      blue =    "#61afef";
+      magenta = "#c678dd";
+      cyan =    "#56b6c2";
+      white =   "#abb2bf";
+    };
+    bright = {
+      black =   "#5c6370";
+      red =     "#e06c75";
+      green =   "#98c379";
+      yellow =  "#d19a66";
+      blue =    "#61afef";
+      magenta = "#c678dd";
+      cyan =    "#56b6c2";
+      white =   "#ffffff";
     };
   };
 in
