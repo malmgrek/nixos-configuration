ead05709fc8318da14a67869983a7455600c96e4
Stratos Staboulis <stratos.staboulis@gmail.com>
2022-07-05 23:18:34 +0300

    Disable Emacs bindings in IPython

--- home/ipython.nix
+++ home/ipython.nix
@@ -7,6 +7,7 @@
   home-manager.users.${config.customParams.userName} = {
     xdg.configFile."ipython/profile_default/ipython_config.py".text = ''
       c.TerminalInteractiveShell.editing_mode = "vi"
+      c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
     '';
   };
 }
