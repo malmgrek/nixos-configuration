a743d9370b20237ea364de7821d278749f59ead6
Stratos Staboulis <stratos.staboulis@gmail.com>
2022-08-12 09:26:07 +0300

    Fix overheating on AC on Lenovo P14s

--- hosts/spyridon/default.nix
+++ hosts/spyridon/default.nix
@@ -28,7 +28,7 @@
       userName = "malmgrek";
     };
     hidpiHacks.enable = true;
-    lightMode.enable = false;
+    lightMode.enable = true;
 
     # Use the systemd-boot EFI boot loader
     # TODO: Try grub
@@ -52,7 +52,19 @@
     services.thermald.enable = false;
     # TLP is a command line for saving laptop battery. TLP will take care of the
     # majority of settings that powertop would enable.
-    services.tlp.enable = true;
+    services.tlp = {
+      enable = true;
+      settings = {
+        # Fixes the problem of overheating and slowing down when on AC.
+        # The `RUNTIME_OM_ON_AC` PCI(e) setting seemed to solely do the trick
+        # but why not save a bit extra energy.
+        AHCI_RUNTIME_PM_ON_AC = "auto";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        RUNTIME_PM_ON_AC = "auto";
+        SCHED_POWERSAVE_ON_AC = "1";
+      };
+    };
     powerManagement.powertop.enable = true;
 
     # TODO: Use declarative style user management with immutable users
